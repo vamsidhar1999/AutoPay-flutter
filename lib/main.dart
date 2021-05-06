cdcdcd5587f83072963d92e58c3b9e7bfa2ba514
Prasanna <prasannabondalapati20@gmail.com>
2021-05-06 20:37:57 +0530

    Added Ac order detail page

--- lib/main.dart
+++ lib/main.dart
@@ -78,7 +78,7 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: ACOrderDetails(),
+        home: SplashScreen(),
       ),
     );
   }
