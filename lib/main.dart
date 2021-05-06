55dde0d5b8a79d923d551db6ba4da798639cccff
Prasanna <prasannabondalapati20@gmail.com>
2021-05-06 19:02:09 +0530

    changed token in Registration page

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class _MyAppState extends State<MyApp> {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: SplashScreen(),
+        home: DashBoard(),
       ),
     );
   }
